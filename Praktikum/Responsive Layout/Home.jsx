f535856bd70936d0e8c922c04e5251124dfa4a15
Naufalzaki05 <100655325+Naufalzaki05@users.noreply.github.com>
2022-12-31 13:32:52 +0700

    Create Home.jsx

---
+++ Praktikum/Responsive Layout/Home.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./home.css";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts" + search);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [search]);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        {posts.length ? (
+          <Posts posts={posts} />
+        ) : (
+          <h1 className="sorryMessage">
+            SORRY : No posts of this category available!
+            <p>:(</p>
+          </h1>
+        )}
+        <Sidebar />
+      </div>
+    </>
+  );
+}
